90d077fece283a158128eaa79723fb39183e32b5
Le Cam <thantoandoc83@gmail.com>
2019-11-23 21:34:59 +0700

    authorized

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,8 +1,9 @@
 const { UserDAO } = require('../models');
 
-const login = async (username, password) => {
+const login = async (username, password, role) => {
   try {
     const user = await UserDAO.findByCredentials(username, password);
+    if(user.role !== role) throw Error('Unauthorized');
     const token = await user.generateToken();
     return { token, user };
   } catch (error) {
